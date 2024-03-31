d3243647203c09f81e026d84ffc477b067b1905b
Tyler Rilling <tyler@underlost.net>
2024-03-30 23:14:21 -0700

    Update colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,19 @@
+import { t } from './dist/_astro/client.DSlHJlSR';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF269E",
+        "primary-light": "#FFDCED",
+        secondary: "#00D7B0",
+        "secondary-light": "#B8FFF3",
+        highlighter: "#FAF77D",
+      },
+    },
   },
   plugins: [require(`precss`), require(`autoprefixer`)],
 };
