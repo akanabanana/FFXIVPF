6f3f879bf6cc5de9132d5e3a8bc60384badfb3fe
Tyler Rilling <tyler@underlost.net>
2024-04-07 11:12:45 -0700

    styling, cleaning up components and TOC

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,28 +5,45 @@ import pagefind from "astro-pagefind";
 import mdx from "@astrojs/mdx";
 import { fileURLToPath } from "url";
 import path from "path";
-import sectionize from "@hbsnow/rehype-sectionize";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import { transformerNotationDiff } from "@shikijs/transformers";
-import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import rehypeSlug from "rehype-slug";
+import expressiveCode from "astro-expressive-code";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+
+// Remark
+import remarkSectionize from "./src/utils/remark/sectionize";
+
 
 const projectRootDir = path.dirname(fileURLToPath(import.meta.url));
 
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind(), mdx(), pagefind()],
+  integrations: [
+    preact(),
+    tailwind(),
+    expressiveCode({
+      theme: "synthwave-84",
+      code: {
+        lineNumbers: true,
+        highlight: true,
+      },
+      plugins: [pluginLineNumbers()],
+    }),
+    mdx(),
+    pagefind(),
+  ],
   markdown: {
-    remarkPlugins: [sectionize],
+    remarkPlugins: [remarkSectionize],
     rehypePlugins: [
       rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: "append" }],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+        },
+      ],
     ],
-    shikiConfig: {
-      //wrap: true,
-      transformers: [transformerNotationDiff()],
-      theme: "synthwave-84",
-    },
   },
   vite: {
     resolve: {
@@ -36,3 +53,4 @@ export default defineConfig({
     },
   },
 });
+
